3d7dab31906f71a9a7e00a13e7ee3be50c933095
writecoffee <writecoffee@gmail.com>
2014-03-30 03:31:32 -0400

    Added configure.sh and generate_cisco_router_tf.sh, modified README

---
+++ configure.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PYTHONPATH=$PYTHONPATH:~/VeriExodus/hsa
+cd hsa
+./setup.sh
---
+++ generate_cisco_router_tf.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd examples/stanford
+mkdir tf_stanford_backbone
+python generate_stanford_backbone_tf.py
