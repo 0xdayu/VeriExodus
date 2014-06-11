a37f8f73b78abd57f594ccb83395f064ea95d19e
Da Yu <da_yu@brown.edu>
2014-06-11 17:07:10 -0400

    Finish decomposition

--- hsa/veriexodus/comparator.py
+++ hsa/veriexodus/comparator.py
@@ -8,7 +8,7 @@ class Comparator:
     
 
     def compare(self, ios_tf, of_tf):
-        
+        pass
 
                 
     def importIOS(self):
@@ -57,18 +57,92 @@ class Comparator:
         of_tf.controller_rules = of_controller_rules
         
         return of_tf
+    
+    @staticmethod
+    def decoupleRules(rules):
+        results = []
+        for rule in rules:
+            temp = []
+            temp.append(rule)
+            for result in results:
+                size = len(temp)
+                for i in range(size): #every decomposed headerspaces
+                    currentRule = temp.pop(0)
+                    intersectPart = wildcard_intersect(currentRule['match'], result['match'])
+
+                    #no intersect parts
+                    if (len(intersectPart) == 0):
+                        temp.append(currentRule)
+                        continue
+                    tempWildcard = wildcard_diff(currentRule['match'], intersectPart)
+                    #if (len(tempWildcard) > 1):
+                    for tw in tempWildcard:
+                        if tw is None:
+                            continue
+                        t = currentRule.copy()
+                        t['match'] = tw
+                            #print '*****1', tw
+                        temp.append(t)
+            results += temp
+        return results
+    
+    @staticmethod
+    def printRules(rules):
+        for rule in rules:
+            if (rule['action'] == 'rw'):
+                print "in_ports: %s, match: %s => ((h & %s) | %s, %s)" % \
+                    (rule['in_ports'], rule['match'], rule['mask'], \
+                     rule['rewrite'], rule['out_ports'])
+
+            if (rule['action'] == 'fwd'):
+                print "in_ports: %s, match: %s => (h , %s)" % \
+                    (rule['in_ports'], rule['match'], rule['out_ports'])
+
+            if (rule['action'] == 'link'):
+                print "in_ports: %s => out_ports: %s" % \
+                    (rule['in_ports'], rule['out_ports'])
+
+            if (rule['action'] == 'custom'):
+                print "match: %s , transform: %s" % \
+                    (rule['match'].__name__, rule['transform'].__name__)
 
 
 if __name__ == "__main__":
     c = Comparator()
     of_tf = c.importOF()
     ios_tf = c.importIOS()
-    print "===============IOS:==================="
+    print "===================IOS-FWD-Rules:==========================="
+    Comparator.printRules(ios_tf.rules)
+    print "===================OF-FWD-Rules:==========================="
+    Comparator.printRules(of_tf.rules)
+    print "===================OF-Controller-Rules:==========================="
+    Comparator.printRules(of_tf.controller_rules)
+    
+    #decomposed
+    
+    ios_tf.rules = Comparator.decoupleRules(ios_tf.rules)
+    of_tf.rules = Comparator.decoupleRules(of_tf.rules)
+    
+    print "================decomposed-IOS-FWD-Rules:===================="
     print ios_tf
-    print "===============OF:===================="
+    print "================decomposed-OF-FWD-Rules:===================="
     print of_tf
     
-    c.compare()
+    '''w1 = wildcard_create_from_string("11111111")
+    w3 = wildcard_create_from_string("1111111x")
+    w2 = wildcard_create_from_string("11111xxx")
     
+    w4 = wildcard_create_from_string("xxxxxxxx")
     
-    #c.compare()
+    rules = TF(1)
+    rules.add_rewrite_rule(TF.create_standard_rule([5], w1, [6], w4, w4))
+    rules.add_rewrite_rule(TF.create_standard_rule([1], w3, [2], w4, w4))
+    rules.add_rewrite_rule(TF.create_standard_rule([3], w2, [4], w4, w4))
+    
+    Comparator.printRules(rules.rules)
+    
+    rules.rules = Comparator.decoupleRules(rules.rules)
+    print "====after====="
+    Comparator.printRules(rules.rules)'''
+    
+    #print re
