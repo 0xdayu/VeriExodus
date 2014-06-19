bffb0226c25048bb23662f7c7e536f9fca668a9e
Da Yu <da_yu@brown.edu>
2014-06-19 18:18:07 -0400

    Finish the comparision with some toy test cases passed

--- hsa/veriexodus/comparator.py
+++ hsa/veriexodus/comparator.py
@@ -8,13 +8,64 @@ class Comparator:
     
     def __init__(self):
         
+        # key, value -> inports number, [rules]
         self.ios_hs = {}
         self.of_hs = {}
 
-    def compare(self, ios_tf, of_tf):
-        pass
-
+    def compare(self, hs1, hs2):
+        isEqual = True
+        for key, value in hs1.iteritems():
+            if key == "controller_rules":
+                print "======Ignore Rules sent to the Controller====="
+                continue
+            if key in hs2.keys():
+                isEqual = isEqual and self.compareRules(self.decoupleRules(value), self.decoupleRules(hs2[key]))
+            else:
+                isEqual = isEqual and False
+        return isEqual
+            
+    def compareRules(self, rules1, rules2):
+        isEqual = True
+        for r1 in rules1:
+            r2 = self.getHeaderSpaceWithSameFourTuples(r1, rules2)
+            if len(r2) == 0:
+                isEqual = False
+                break
+            
+            temp = []
+            temp.append(r1)
+            for _r2 in r2:
+                for i in range(len(temp)):
+                    _r1 = temp.pop(0)
+                    intersect_part = wildcard_intersect(_r1["match"], _r2["match"])
+                    if len(intersect_part) == 0: #no overlap headerspace
+                        temp.insert(0, _r1)
+                        continue
+                    
+                    substracted_match = wildcard_diff(_r1["match"], intersect_part)
+                    for sm in substracted_match:
+                        if sm is None:
+                            continue
+                        t = _r1.copy()
+                        t["match"] = sm
+                        temp.append(t)
+            
+            if len(temp) != 0:
+                isEqual = False
+        
+        #print "----", isEqual
+        return isEqual
+                             
+    def getHeaderSpaceWithSameFourTuples(self, r1, rule_set): # get rules from rule_set with same inports, mask, rewrite and outports
+        result = []
+        for r2 in rule_set:
+            if wildcard_is_equal(r1["mask"], r2["mask"]) and \
+            wildcard_is_equal(r1["rewrite"], r2["rewrite"]) and \
+            set(r1["out_ports"]) == set(r2["out_ports"]):
+                result.append(r2)
                 
+        return result
+        
     def importIOS(self):
         example_folder = '../examples/Exodus_toy_example/'
         ios = cisco_router(1)
@@ -42,7 +93,6 @@ class Comparator:
                 else:
                     self.ios_hs[inports] = [rule]
         
-    
     def importOF(self):
         
         of_tf = generate_ext()
@@ -78,8 +128,7 @@ class Comparator:
                     else:
                         self.of_hs[inports] = [rule]'''
     
-    @staticmethod
-    def decoupleRules(rules):
+    def decoupleRules(self, rules):
         results = []
         for rule in rules:
             temp = []
@@ -107,7 +156,7 @@ class Comparator:
     @staticmethod
     def printRules(rules):
         for key,value in rules.iteritems():
-            print "============%s====================="% key
+            print "================Bucket: %s================="% key
             for rule in value:
                 if (rule['action'] == 'rw'):
                     print "in_ports: %s, match: %s => ((h & %s) | %s, %s)" % \
@@ -125,44 +174,61 @@ class Comparator:
                 if (rule['action'] == 'custom'):
                     print "match: %s , transform: %s" % \
                         (rule['match'].__name__, rule['transform'].__name__)
+                        
+    # Just for Test                        
+    def TestDictGen(self, rules):
+        d = {}
+        for rule in rules:
+            for ports in rule["in_ports"]:
+                if ports in d.keys():
+                    '''r = rule.copy()
+                    r["in_ports"] = [ports]
+                    d[ports] += [r]'''
+                    d[ports] += [rule]
+                else:
+                    '''r = rule.copy()
+                    r["in_ports"] = [ports]
+                    d[ports] = [r]'''
+                    d[ports] = [rule]
+        return d
 
 if __name__ == "__main__":
-    c = Comparator()
+    ''''c = Comparator()
     c.importOF()
     c.importIOS()
     print "===================IOS-FWD-Rules:==========================="
     c.printRules(c.ios_hs)
     print "===================OF-FWD-Rules:==========================="
-    c.printRules(c.of_hs)
-    #print "===================OF-Controller-Rules:==========================="
-    #Comparator.printRules(of_tf.controller_rules)
+    c.printRules(c.of_hs)'''
     
-    #decomposed
+    #Test Case
+    w1 = wildcard_create_from_string("11111111")
+    w2 = wildcard_create_from_string("11111110")
+    w3 = wildcard_create_from_string("1111111x")
+    w4 = wildcard_create_from_string("11111xxx")
     
-    '''ios_tf.rules = Comparator.decoupleRules(ios_tf.rules)
-    of_tf.rules = Comparator.decoupleRules(of_tf.rules)
+    w5 = wildcard_create_from_string("xxxxxxxx")
     
-    print "================decomposed-IOS-FWD-Rules:===================="
-    print ios_tf
-    print "================decomposed-OF-FWD-Rules:===================="
-    print of_tf'''
+    r1 = TF(1)
+    r1.add_rewrite_rule(TF.create_standard_rule([1], w1, [6], w5, w5))
+    r1.add_rewrite_rule(TF.create_standard_rule([1], w2, [6], w5, w5))
+    r1.add_rewrite_rule(TF.create_standard_rule([1], w4, [6], w5, w5))
+    r1.add_rewrite_rule(TF.create_standard_rule([3,1], w2, [4], w5, w5))
     
-    '''w1 = wildcard_create_from_string("11111111")
-    w3 = wildcard_create_from_string("11111110")
-    w2 = wildcard_create_from_string("11111xxx")
+    r2 = TF(1)
+    r2.add_rewrite_rule(TF.create_standard_rule([1], w3, [6], w5, w5))
+    r2.add_rewrite_rule(TF.create_standard_rule([1], w4, [6], w5, w5))
+    r2.add_rewrite_rule(TF.create_standard_rule([3], w2, [4], w5, w5))
     
-    print wildcard_and(w3,w1)
-    print wildcard_or(w3,w1)
+    c = Comparator()
+    r1 = c.TestDictGen(r1.rules)
+    r2 = c.TestDictGen(r2.rules)
+    print "========TF1========="
+    c.printRules(r1)
+    print "========TF2========="
+    c.printRules(r2)
     
-    w4 = wildcard_create_from_string("xxxxxxxx")
-    
-    rules = TF(1)
-    rules.add_rewrite_rule(TF.create_standard_rule([5], w1, [6], w4, w4))
-    rules.add_rewrite_rule(TF.create_standard_rule([1], w3, [2], w4, w4))
-    rules.add_rewrite_rule(TF.create_standard_rule([3], w2, [4], w4, w4))
-    
-    Comparator.printRules(rules.rules)
-    
-    rules.rules = Comparator.decoupleRules(rules.rules)
-    print "====after====="
-    Comparator.printRules(rules.rules)'''
+    if c.compare(r1, r2):
+        print "TF1 equals to TF2"
+    if c.compare(r2, r1):
+        print "TF2 equals to TF1"
